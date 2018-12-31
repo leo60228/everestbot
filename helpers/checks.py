53d807281e9472aea2ab1692683ab5e0ab16ad91
leo60228 <iakornfeld@gmail.com>
2018-12-30 19:15:08 -0500

    disable checks

--- helpers/checks.py
+++ helpers/checks.py
@@ -2,20 +2,12 @@ import config
 
 
 def check_if_staff(ctx):
-    if not ctx.guild:
-        return False
-    return any(r.id in config.staff_role_ids for r in ctx.author.roles)
+    return True
 
 
 def check_if_bot_manager(ctx):
-    if not ctx.guild:
-        return False
-    return any(r.id == config.bot_manager_role_id for r in ctx.author.roles)
+    return True
 
 
 def check_if_staff_or_ot(ctx):
-    if not ctx.guild:
-        return True
-    is_ot = (ctx.channel.name == "off-topic")
-    is_staff = any(r.id in config.staff_role_ids for r in ctx.author.roles)
-    return (is_ot or is_staff)
+    return True
