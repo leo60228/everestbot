551defa9714b5a22feb7ccfea226527086ce01bc
leo60228 <iakornfeld@gmail.com>
2018-12-30 19:52:00 -0500

    bring back some checks

--- helpers/checks.py
+++ helpers/checks.py
@@ -1,13 +1,14 @@
 import config
 
-
 def check_if_staff(ctx):
-    return True
+    return False
 
 
 def check_if_bot_manager(ctx):
-    return True
-
+    return False
 
 def check_if_staff_or_ot(ctx):
-    return True
+    if not ctx.guild:
+        return True
+    is_meme = (ctx.channel.name == "meme_line" || ctx.channel.name == "modding_irl")
+    return is_meme
