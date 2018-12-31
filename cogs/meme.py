8164d8d688bace55b1aa66eeb19629a8d711331d
leo60228 <iakornfeld@gmail.com>
2018-12-30 20:01:19 -0500

    bowling is no fun D:

--- cogs/meme.py
+++ cogs/meme.py
@@ -21,45 +21,45 @@ class Meme:
         """this is where we take memes REALLY far"""
         return math.floor(c + 273.15)
 
-    @commands.check(check_if_staff_or_ot)
-    @commands.command(hidden=True, name="warm")
-    async def warm_member(self, ctx, user: discord.Member):
-        """Warms a user :3"""
-        celsius = random.randint(15, 100)
-        fahrenheit = self.c_to_f(celsius)
-        kelvin = self.c_to_k(celsius)
-        await ctx.send(f"{user.mention} warmed."
-                       f" User is now {celsius}°C "
-                       f"({fahrenheit}°F, {kelvin}K).")
+    #@commands.check(check_if_staff_or_ot)
+    #@commands.command(hidden=True, name="warm")
+    #async def warm_member(self, ctx, user: discord.Member):
+    #    """Warms a user :3"""
+    #    celsius = random.randint(15, 100)
+    #    fahrenheit = self.c_to_f(celsius)
+    #    kelvin = self.c_to_k(celsius)
+    #    await ctx.send(f"{user.mention} warmed."
+    #                   f" User is now {celsius}°C "
+    #                   f"({fahrenheit}°F, {kelvin}K).")
 
-    @commands.check(check_if_staff_or_ot)
-    @commands.command(hidden=True, name="chill", aliases=["cold"])
-    async def chill_member(self, ctx, user: discord.Member):
-        """Chills a user >:3"""
-        celsius = random.randint(-50, 15)
-        fahrenheit = self.c_to_f(celsius)
-        kelvin = self.c_to_k(celsius)
-        await ctx.send(f"{user.mention} chilled."
-                       f" User is now {celsius}°C "
-                       f"({fahrenheit}°F, {kelvin}K).")
+    #@commands.check(check_if_staff_or_ot)
+    #@commands.command(hidden=True, name="chill", aliases=["cold"])
+    #async def chill_member(self, ctx, user: discord.Member):
+    #    """Chills a user >:3"""
+    #    celsius = random.randint(-50, 15)
+    #    fahrenheit = self.c_to_f(celsius)
+    #    kelvin = self.c_to_k(celsius)
+    #    await ctx.send(f"{user.mention} chilled."
+    #                   f" User is now {celsius}°C "
+    #                   f"({fahrenheit}°F, {kelvin}K).")
 
-    @commands.check(check_if_staff_or_ot)
-    @commands.command(hidden=True)
-    async def bones(self, ctx):
-        await ctx.send("https://cdn.discordapp.com/emojis/"
-                       "443501365843591169.png?v=1")
+    #@commands.check(check_if_staff_or_ot)
+    #@commands.command(hidden=True)
+    #async def bones(self, ctx):
+    #    await ctx.send("https://cdn.discordapp.com/emojis/"
+    #                   "443501365843591169.png?v=1")
 
-    @commands.check(check_if_staff_or_ot)
-    @commands.command(hidden=True, name="bam")
-    async def bam_member(self, ctx, user: discord.Member):
-        """Bams a user owo"""
-        await ctx.send(f"{self.bot.escape_message(user)} is ̶n͢ow b̕&̡.̷ 👍̡")
+    #@commands.check(check_if_staff_or_ot)
+    #@commands.command(hidden=True, name="bam")
+    #async def bam_member(self, ctx, user: discord.Member):
+    #    """Bams a user owo"""
+    #    await ctx.send(f"{self.bot.escape_message(user)} is ̶n͢ow b̕&̡.̷ 👍̡")
 
-    @commands.check(check_if_staff_or_ot)
-    @commands.command(hidden=True)
-    async def memebercount(self, ctx):
-        """Checks memeber count, as requested by dvdfreitag"""
-        await ctx.send("There's like, uhhhhh a bunch")
+    #@commands.check(check_if_staff_or_ot)
+    #@commands.command(hidden=True)
+    #async def memebercount(self, ctx):
+    #    """Checks memeber count, as requested by dvdfreitag"""
+    #    await ctx.send("There's like, uhhhhh a bunch")
 
     @commands.check(check_if_staff_or_ot)
     @commands.command(hidden=True)
